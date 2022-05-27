67d37c10681cf457ffcfe4c2bb8e2ea8aaf57c60
Darren Potter <darpots@outlook.com>
2022-05-27 11:41:19 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('.email');
+const message = document.querySelector('.invalid-email');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function (e) {
+  e.preventDefault();
+  input.classList.remove('error-input');
+  message.classList.remove('error-message');
+
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!input.value.match(emailRegex)) {
+    input.classList.add('error-input');
+    message.classList.add('error-message');
+  }
+});
